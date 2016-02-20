89917a1eecb794dca50db378ed894ef59723824d
elschan <elschan90@gmail.com>
2016-02-20 13:22:17 -0800

    comment

--- web.rb
+++ web.rb
@@ -52,7 +52,7 @@ post '/move' do
 
     # Identify us
     boomslang = requestJson["snakes"].detect { |snake| snake[:id] == "76bbcf39-5b5e-4888-a3a7-808c88fb8126" }
-    getCoordinates(boomslang[:coords][0])
+    # getCoordinates(boomslang[:coords][0])
 
     # Our next move is not towards a wall
     # Identify possible moves
