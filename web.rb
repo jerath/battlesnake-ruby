9a1db0ed628736044a144555406c9f193807ba5e
Jessica Thomas <jess.rae.thomas@gmail.com>
2016-02-20 14:50:25 -0800

    booms

--- web.rb
+++ web.rb
@@ -83,7 +83,7 @@ post '/move' do
     puts requestJson
 
     # Identify us
-    # boomslang = requestJson["snakes"].detect { |snake| snake[:id] == "76bbcf39-5b5e-4888-a3a7-808c88fb8126" }
+    boomslang = requestJson["snakes"].detect { |snake| snake[:id] == "76bbcf39-5b5e-4888-a3a7-808c88fb8126" }
 
     # getAdjacentCoordinates(boomslang[:coords][0])
 
